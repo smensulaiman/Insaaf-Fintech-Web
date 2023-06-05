ab9f5835d249a70b336be3df134180a5619d82e8
Mohammad Sulaiman <smensulaiman007@gmail.com>
2023-06-05 09:24:45 +0900

    spring boot init

---
+++ src/main/java/jp/insaaf/fintech/InsaafFintechApplication.java
@@ -0,0 +1,13 @@
+package jp.insaaf.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaafFintechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsaafFintechApplication.class, args);
+    }
+
+}
